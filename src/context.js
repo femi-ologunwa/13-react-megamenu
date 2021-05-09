b906ef452b50268747c42f911ed89d65ca6437c3
Femi <ologunwafemi@gmail.com>
2021-05-09 17:30:51 +0100

    setup context data exchange structure

--- src/context.js
+++ src/context.js
@@ -1,1 +1,41 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = (props) => {
+   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+   const [isMegamenuOpen, setIsMegamenuOpen] = useState(true);
+
+   const openSidebar = () => {
+      setIsSidebarOpen(true);
+   };
+
+   const closeSidebar = () => {
+      setIsMegamenuOpen(false);
+   };
+
+   const openMegamenu = () => {
+      setIsMegamenuOpen(true);
+   };
+
+   const closeMegamenu = () => {
+      setIsMegamenuOpen(false);
+   };
+
+   return (
+      <AppContext.Provider
+         value={{
+            isMegamenuOpen,
+            isSidebarOpen,
+            openMegamenu,
+            openSidebar,
+            closeMegamenu,
+            closeSidebar,
+         }}
+      >
+         {props.children}
+      </AppContext.Provider>
+   );
+};
+
+export { AppContext, AppProvider };
