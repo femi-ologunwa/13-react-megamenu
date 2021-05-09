e01d77d9276f648f21ff492540e2edb4ef857262
Femi <ologunwafemi@gmail.com>
2021-05-10 00:30:21 +0100

    added functionality to show/hide megamenu submenu - 4
    depending on the number of links, we want to change the number of columns in (and width of) the container

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { AppContext } from './context';
 
 const Submenu = () => {
@@ -11,12 +11,23 @@ const Submenu = () => {
    //setup useRef to target the submegamenu container so as to add some styling to it.
    const container = useRef(null);
 
+   const [columns, setColumns] = useState('col-2');
+
    useEffect(() => {
+      setColumns('col-2');
       const submegamenu = container.current;
       const { center, bottom } = location;
       submegamenu.style.left = `${center}px`;
       submegamenu.style.top = `${bottom}px`;
-   }, [location]);
+
+      if (links.length === 3) {
+         setColumns('col-3');
+      }
+
+      if (links.length > 3) {
+         setColumns('col-4');
+      }
+   }, [location, links]);
 
    return (
       <aside
@@ -24,7 +35,7 @@ const Submenu = () => {
          ref={container}
       >
          <h4>{page}</h4>
-         <div className={`submenu-center col-2`}>
+         <div className={`submenu-center ${columns}`}>
             {links.map((link, index) => {
                const { label, icon, url } = link;
                return (
