1fdd1eccde9eb816a1c65cfaab5d3d55b76e62df
Femi <ologunwafemi@gmail.com>
2021-05-10 04:32:03 +0100

    Updates

---
+++ precache-manifest.f48c8be60cdac53d1f17a2138d80271b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2ae290d00d0f27816d7755a2ca1ef0",
+    "url": "/13-react-megamenu/index.html"
+  },
+  {
+    "revision": "eb2dc700267fcadd5df3",
+    "url": "/13-react-megamenu/static/css/main.09a9789c.chunk.css"
+  },
+  {
+    "revision": "900723df6c983d96b741",
+    "url": "/13-react-megamenu/static/js/2.45f9593c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/13-react-megamenu/static/js/2.45f9593c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2dc700267fcadd5df3",
+    "url": "/13-react-megamenu/static/js/main.dfe20533.chunk.js"
+  },
+  {
+    "revision": "a27c1251613664148946",
+    "url": "/13-react-megamenu/static/js/runtime-main.ef7d5454.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/13-react-megamenu/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/13-react-megamenu/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/13-react-megamenu/static/media/phone.58d7e3d6.svg"
+  }
+]);
